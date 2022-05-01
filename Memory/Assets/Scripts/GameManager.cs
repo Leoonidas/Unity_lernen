c4a855e947af672020d005768db2765dc0c347ef
Leoonidas <leonadelhuette@gmx.de>
2022-05-01 20:50:23 +0200

    Update 1.5. Anheben und Rotation der Karten

--- Memory/Assets/Scripts/GameManager.cs
+++ Memory/Assets/Scripts/GameManager.cs
@@ -7,9 +7,26 @@ public class GameManager : MonoBehaviour
     public MemoryCard FirstSelectedCard;
     public MemoryCard SecondSelectedCard;
 
+    private bool CanClick = true;
+
 
     public void CardClicked(MemoryCard Card)
     {
+        // Verhindern von Klicken wärend dem Warten auf das Umdrehen
+        if (CanClick == false)
+        {
+            return;
+        }
+
+
+
+        // Anheben der Karte
+        Card.TargetHight = 0.1f;
+        // Rotation der Karte
+        Card.TargetRoation = 90;
+
+
+        // Unterscheidung, welche Karte angeklickt wurde und ob ein Paar umgedreht wurde
         if (FirstSelectedCard == null)
         {
             FirstSelectedCard = Card;
@@ -20,15 +37,35 @@ public class GameManager : MonoBehaviour
             SecondSelectedCard = Card;
             Debug.Log(Card.Identifier);
 
-            if (FirstSelectedCard.Identifier == SecondSelectedCard.Identifier)
-            {
-                Destroy(FirstSelectedCard.gameObject);
-                Destroy(SecondSelectedCard.gameObject);
-            }
+            CanClick = false;
 
-            FirstSelectedCard = null;
-            SecondSelectedCard = null;
+            // Verzögerung der Ausführung von CheckMatch()
+            Invoke("CheckMatch", 1);
+
         }
         
+    }
+
+    // Überprüft, ob die zwei ausgewählten Karten ein Paar ergeben
+    private void CheckMatch()
+    {
+        if (FirstSelectedCard.Identifier == SecondSelectedCard.Identifier)
+        {
+            Destroy(FirstSelectedCard.gameObject);
+            Destroy(SecondSelectedCard.gameObject);
+        }
+        else
+        {
+            // Wiederumdrehen der Karten
+            FirstSelectedCard.TargetRoation = -90;
+            SecondSelectedCard.TargetRoation = -90;
+            // Absetzen der Karten
+            FirstSelectedCard.TargetHight = 0.01f;
+            SecondSelectedCard.TargetHight = 0.01f;
+        }
+        FirstSelectedCard = null;
+        SecondSelectedCard = null;
+
+        CanClick = true;
     }
 }
--- Memory/Assets/Scripts/MemoryCard.cs
+++ Memory/Assets/Scripts/MemoryCard.cs
@@ -6,13 +6,26 @@ public class MemoryCard : MonoBehaviour
 {
     public int Identifier;
 
+    public float TargetHight = 0.01f;
+    public int TargetRoation = -90;
+
+
     public void OnMouseDown()
     {
         // 
         Debug.Log("Clicked");
         FindObjectOfType<GameManager>().CardClicked(this);
-        
+    }
 
+    private void Update()
+    {
+        // Bewegung der Karte nach oben oder unten, als die TargetHeight vom GameManager geändert wird
+        float HeightValue = Mathf.MoveTowards(transform.position.y, TargetHight, 1 * Time.deltaTime);
+        transform.position = new Vector3 (transform.position.x, HeightValue, transform.position.z);
+
+        // smoothe Rotation über lerb(), wenn die Targetrotation vom GameManager geändert wird
+        Quaternion RotationValue = Quaternion.Euler(TargetRoation, 0, 0);
+        transform.rotation = Quaternion.Lerp(transform.rotation, RotationValue, 8 * Time.deltaTime);
     }
 
 }

a4ce6d5376e81cf0c8daa83cfe91ff4979778379
lenooonidas <leonadelhuette@gmx.de>
2022-05-28 12:39:46 +0200

    Terrain, Character Model und Animationen

--- RPG/Assets/Scripts/CharacterMovement.cs
+++ RPG/Assets/Scripts/CharacterMovement.cs
@@ -7,7 +7,7 @@ public class CharacterMovement : MonoBehaviour
 {
 
     public NavMeshAgent agent;
-
+    public Animator anim;
 
 
     // Start is called before the first frame update
@@ -19,20 +19,24 @@ public class CharacterMovement : MonoBehaviour
 
     private void Update()
     {
+        // Linksklick
         if (Input.GetMouseButtonDown(0))
         {
+
             Ray mousePos = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
+            // Falls ein gebaketes Objekte angeklickt wird
             if (Physics.Raycast(mousePos, out hit))
             {
-
-
+                // Bewegung
                 agent.SetDestination(hit.point);
             }
-            
         }
-        
-    }
 
+        // falls der velocity Vektor größers als 0 ist, läuft der Charater
+        bool isRunning = agent.velocity.magnitude > 0;
+        anim.SetBool("isRunning", isRunning);
+
+    }
 }
